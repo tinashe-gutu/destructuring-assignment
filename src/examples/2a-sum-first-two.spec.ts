4efbd7ef0804aca094a81511b89dc393910b6494
Richard Ng <richardcrng@gmail.com>
2021-06-10 14:19:47 +0100

    Fix test file

--- src/examples/2a-sum-first-two.spec.ts
+++ src/examples/2a-sum-first-two.spec.ts
@@ -1,6 +1,6 @@
 import sumFirstTwo from "./2a-sum-first-two";
 
-describe("sumFirstTwo adds together only the first two numbers in an array", () => {
+test("sumFirstTwo adds together only the first two numbers in an array", () => {
   expect(sumFirstTwo([3, 9, 2])).toBe(12);
   expect(sumFirstTwo([3, 2, 9])).toBe(5);
   expect(sumFirstTwo([1, 1, 100, 100])).toBe(2);
