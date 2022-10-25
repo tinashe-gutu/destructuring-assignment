a75118f805e6c625a196bef60cb2f1a3d95c8aa0
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-10-25 21:43:19 +0100

    object destructuring 0b

--- src/challenges/0b-object-destructuring.ts
+++ src/challenges/0b-object-destructuring.ts
@@ -6,9 +6,11 @@ const country = {
 };
 
 // TODO: use object destructuring
-const abbreviation = country.abbreviation;
-const capital = country.capital;
-const population = country.population;
+//const abbreviation = country.abbreviation;
+//const capital = country.capital;
+//const population = country.population;
+
+const {abbreviation, capital, population} = country;
 
 // export so the values can be tested - but you can ignore this
 export { abbreviation, capital, population };
